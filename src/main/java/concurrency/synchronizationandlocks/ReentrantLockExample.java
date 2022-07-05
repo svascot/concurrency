0888d4548971054c402576ccca271e2ad4eda4e9
svascot <svascot@gmail.com>
2022-07-05 11:06:28 -0500

    Example to understand ReentrantLock

--- src/main/java/concurrency/synchronizationandlocks/ReentrantLockExample.java
+++ src/main/java/concurrency/synchronizationandlocks/ReentrantLockExample.java
@@ -3,11 +3,13 @@ package concurrency.synchronizationandlocks;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
 
 import static concurrency.utils.ConcurrentUtils.sleep;
 import static concurrency.utils.ConcurrentUtils.stop;
 
 public class ReentrantLockExample {
+    static int count = 0;
 
     public static void main(String[] args) {
         ExecutorService executor = Executors.newFixedThreadPool(2);
@@ -32,5 +34,28 @@ public class ReentrantLockExample {
         });
 
         stop(executor);
+
+        testIncrementCount();
+    }
+
+    // Example working with locks to prevent a race condition working over a counter.
+    private static void testIncrementCount(){
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        ReentrantLock lock = new ReentrantLock();
+
+        // In this example, the counter should be increased by 10.000
+        // With the lock implementation it avoids the race condition, otherwise
+        // the counter will not reach the expected value.
+        IntStream.range(0,10000).forEach(i-> executor.submit(()-> {
+            lock.lock();
+            try{
+                count++;
+            } finally {
+                lock.unlock();
+            }
+        }));
+
+        stop(executor);
+        System.out.println("Counter: " + count);
     }
 }
